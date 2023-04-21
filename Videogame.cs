ace47e65b8881071185119f13d6dd76c47bf7195
Francesco Limpias <francescolimpias@gmail.com>
2023-04-21 14:22:39 +0200

    Added Videogame class

---
+++ Videogame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    public class Videogame
+    {
+        /* ***************
+         * VIDEOGAME's PROPERTIES
+         */
+        [Key] public long Id { get; set; }
+        public string Name { get; set; }
+
+        //CONSTRUCTOR
+        internal Videogame(long id, string name)
+        {
+            Id = id;
+
+            if (name.Length > 50)
+                throw new ArgumentException($"The given name is too long (max: 50) [{name}]");
+            Name = name;
+        }
+
+        /* ***************
+         * VIDEOGAME's METHODS
+         */
+    }
+}
