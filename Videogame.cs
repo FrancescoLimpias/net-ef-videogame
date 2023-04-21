5d826132a0113802780332fa848158842c3c2a15
Francesco Limpias <francescolimpias@gmail.com>
2023-04-21 14:47:25 +0200

    Refactored Insert & SearchById @ VideogamesManager

--- Videogame.cs
+++ Videogame.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace net_ef_videogame
 {
@@ -16,10 +11,8 @@ namespace net_ef_videogame
         public string Name { get; set; }
 
         //CONSTRUCTOR
-        internal Videogame(long id, string name)
+        internal Videogame(string name)
         {
-            Id = id;
-
             if (name.Length > 50)
                 throw new ArgumentException($"The given name is too long (max: 50) [{name}]");
             Name = name;
---
+++ VideogamesManager.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace net_ef_videogame
+{
+    internal static class VideogamesManager
+    {
+
+        //Connection (just a ref to Program.DB)
+        internal static TournamentContext DB = Program.DB;
+
+        internal static bool Insert(string videogameName)
+        {
+            Videogame newVideogame = new(videogameName);
+
+            DB.Videogames.Add(newVideogame);
+
+            return DB.SaveChanges() == 1;
+        }
+
+        internal static Videogame? SearchById(long id)
+        {
+            return DB.Videogames.Find(id);
+        }
+
+        internal static List<Videogame> SearchByName(string name)
+        {
+            List<Videogame> videogames = new List<Videogame>();
+
+            string query = "SELECT * FROM videogames WHERE UPPER(name) LIKE @Name";
+
+            SqlCommand command = new SqlCommand(query, Program.SQL);
+            command.Parameters.AddWithValue("@Name", $"%{name.ToUpper()}%");
+
+            using SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                videogames.Add(new Videogame(reader.GetInt64(0), reader.GetString(1)));
+            }
+
+            return videogames;
+        }
+
+        internal static bool Delete(long id)
+        {
+            string query = "DELETE FROM videogames WHERE id = @Id";
+
+            using SqlCommand command = new SqlCommand(query, Program.SQL);
+            command.Parameters.AddWithValue("@Id", id);
+
+            return command.ExecuteNonQuery() > 0;
+        }
+
+        internal static List<Videogame> List()
+        {
+            List<Videogame> videogames = new();
+            string selectionQuery = "SELECT * FROM videogames";
+
+            //Build SQL command
+            using SqlCommand command = Program.SQL.CreateCommand();
+            command.Connection = Program.SQL;
+            command.CommandText = selectionQuery;
+
+            using SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                videogames.Add(new Videogame(reader.GetInt64(0), reader.GetString(1)));
+            }
+
+            return videogames;
+        }
+    }
+}
